1964f5f996e2b2717561ce432f6bb61929967301
Yeni <yeni_jerke@hotmail.com>
2023-06-20 20:12:51 -0300

    agregamos un navegador

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ let password = prompt(`Ingrese su Password`)
 console.log(user)
 console.log(password)
 
-if (user == `yeni` && password == `1122`) {
+if (user == `marcos` && password == `1122`) {
     alert(`Bienvenido a su cuenta`)
     location.href = `ingreso.html`
 }else{
